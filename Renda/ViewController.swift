af98cd6df6f7e5ecb4b0c018bc92210986b7943a
yamaokana <kudanobo11@outlook.jp>
2022-08-28 04:57:48 +0900

    よくわからないので一旦コミット

--- Renda/ViewController.swift
+++ Renda/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -16,15 +17,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tapButton.layer.cornerRadius = 125
+        firestore.collection("counts").document("share").addSnapshotListener{ snapshot, error in
+            if error != nil {
+                print("エラー発生")
+                print(error)
+                return
+            }
+            let data = snapshot?.data()
+            if data == nil{
+                print("データがありません")
+                return
+            }
+            let count = data!["count"] as? Int
+            if count == nil{
+                print("countという対応する値がありません")
+                return
+            }
+            self.tapCount = count!
+            self.countLabel.text = String(count!)
+        }
     }
+    let firestore = Firestore.firestore()
     @IBAction func tapTapButton(){
         tapCount = tapCount + 1
         countLabel.text = String(tapCount)
-        if tapCount >= 10{
-            countLabel.textColor = UIColor.red
-        }else{
-            countLabel.textColor = UIColor.black
-        }
+        firestore.collection("counts").document("share").setData(["count": tapButton])
     }
 }
 
